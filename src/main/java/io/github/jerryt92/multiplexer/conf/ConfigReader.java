1459df1bcbbbf8f3185d82a35ea623ff3afc3604
tianjingli <tianjingli@raisecom.com>
2025-04-17 19:40:29 +0800

    refactor: 移除多余代码

--- src/main/java/io/github/jerryt92/multiplexer/conf/ConfigReader.java
+++ src/main/java/io/github/jerryt92/multiplexer/conf/ConfigReader.java
@@ -8,7 +8,6 @@ import lombok.Getter;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.config.plugins.convert.TypeConverters;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.InputStream;
